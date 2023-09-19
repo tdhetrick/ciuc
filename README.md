5146bfff6b2bc7b07a23914e124d5c64ca979169
Todd Hetrick <me@toddhetrick.com>
2023-09-19 08:21:13 -0400

    attempt at comparing all code real time

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # ciuc
 This project is to create a tool to allow teachers to verify if students are coding within expected patters in an efforts to reduce acidemic integrity issues.
 
+## Week 02
+I have developed a chrome extension that can monitor a html element for changes. This has been designed to with the codingrooms.com editor. This editor's behavior is not like that of a typical input element. It is a div with a series of spans representing the lines in the editor. When a line is clicked on it becomes edit able. Lines also disappear and reappear as the the window is scrolled over the code. 
+
+The result is that not all of the code necessarily is in the html at one time. This is not so much an issue when monitoring changes, but does keep you from having a full view of all the code at the moment of change. Large changes can be detected, and activity monitored. 
+
+
+
 
--- chrome_ext/content.js
+++ chrome_ext/content.js
@@ -1,6 +1,9 @@
-// Function to handle mutations
 
-var codeRoot;
+var codeRoot = null;
+var prevCode = null;
+const config = { attributes: true, childList: true, characterData: true, subtree: true };
+
+
 
 function handleDivMutations(mutationsList, observer) {
     for(let mutation of mutationsList) {
@@ -8,33 +11,152 @@ function handleDivMutations(mutationsList, observer) {
             if (mutation.target.textContent.includes("TRACKME123")) {
                 console.log("***Started monitoring this code div.");
                 console.log(mutation.target);
-                divObserver.disconnect();
+                observer.disconnect();
                 codeRoot = mutation.target;
                 codeObserver.observe(mutation.target, config);
+                mutation.target.addEventListener('scroll', (event) => {
+                    console.log('The content div was scrolled!');
+                    // You can use event.target.scrollTop to know the scroll position
+                  });
+                  
             }
         }
     }
 }
 
 function handleCodeMutations(mutationsList, observer) {
-    console.log("***Code changed", codeRoot.textContent);
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList' || mutation.type === 'characterData') {
-            //console.log("***Code changed", mutation.target.textContent);
 
+ 
+        let currentCode = "";
+
+        //currentCode = getSpanLines(codeRoot);
+
+        //currentCode = codeRoot.textContent;
+
+        //console.log("current: " + currentCode);
+        var levCount  = 0;
+        var lineDiff = 0;
+
+        for(let mutation of mutationsList) {
+
+            currentCode = getSpanLines(codeRoot);
+
+            console.log("mut: "+mutation.target.textContent);
+
+            if (currentCode && prevCode){
+                pcode = prevCode.replace(/[^a-zA-Z\s\*]/g, '');
+                ccode = currentCode.replace(/[^a-zA-Z\s\*]/g, '');
+                //if (pcode != ccode ){
+                    console.log(pcode);
+                    console.log(ccode);
+                    levCount += levenshtein(pcode,ccode);
+                    lineDiff += diffLines(pcode,ccode); 
+                   
+                //}
+            
+                //let lnCount = countDiffLines(prevCode,currentCode);
+                console.log("Lev: "+levCount + " lineDiff: " +lineDiff); 
+                
+            }
+
+            prevCode = currentCode;
+
+            
         }
-    }
+             
+        //console.log("Lev: "+levCount + " lineDiff: " +lineDiff); 
 
 }
-// Create a new divObserver instance with the callback
-let divObserver = new MutationObserver(handleDivMutations);
 
+// function extractTextWithDelimiter(element, delimiter) {
+//     let childSpans = Array.from(element.children).filter(child => child.tagName === "SPAN");
+//     let texts = childSpans.map(span => span.textContent);
+//     return texts.join(delimiter);
+// }
+
+
+let divObserver = new MutationObserver(handleDivMutations);
 let codeObserver = new MutationObserver(handleCodeMutations);
 
-// Options for the divObserver (which mutations to observe)
-const config = { attributes: true, childList: true, characterData: true, subtree: true };
 
-// Start observing all div elements for configured mutations
+
 document.querySelectorAll('div').forEach(div => {
     divObserver.observe(div, config);
 });
+
+function getSpanLines(element, delimiter = "*l*") {
+
+    const lines = Array.from(element.querySelectorAll('div')).map(div => div.textContent);
+
+    return lines.join(delimiter);
+}
+
+
+function diffLines(pcode, ccode) {
+
+    const prev = new Set(pcode.split("*l*"));
+    const current = new Set(ccode.split("*l*"));
+
+    console.log("size: "+current.size + " "+ prev.size);
+                    
+    
+    diffCount = current.size - prev.size;
+
+    return diffCount;
+}
+function countDiffLines(pcode, ccode) {
+
+    // pcode = pcode.replace(/[^a-zA-Z0-9|]/g, '');
+    // ccode = ccode.replace(/[^a-zA-Z0-9|]/g, '');
+
+    const prev = new Set(pcode.split("|"));
+    const current = new Set(ccode.split("|"));
+    
+    let diffCount = 0;
+
+    // Check for lines in text1 that aren't in text2
+    for (const pline of prev) {
+        if (!current.has(pline)) {
+            console.log("Prev line "+pline+ "removed form current")
+            diffCount++;
+        }
+    }
+
+    // Check for lines in text2 that aren't in text1
+    for (const cline of current) {
+        if (!prev.has(cline)) {
+            console.log("Curent line "+cline+ "added to Prev")
+            diffCount++;
+        }
+    }
+    
+    return diffCount;
+}
+
+function levenshtein(a, b) {
+    const matrix = [];
+    let i, j;
+
+    if (a.length == 0) return b.length;
+    if (b.length == 0) return a.length;
+
+    for (i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+    for (j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) == a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
+                    Math.min(matrix[i][j - 1] + 1,   // insertion
+                    matrix[i - 1][j] + 1));  // deletion
+            }
+        }
+    }
+    return matrix[b.length][a.length];
+}
+
